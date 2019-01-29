7bb6cbbf285fad11a6e01e1cfacc22eeb2ccaefc
brayanmcz <brayan.mcz@gmail.com>
2019-01-28 19:38:19 -0800

    test

--- includes/libft/added_functions/libft_include.h
+++ includes/libft/added_functions/libft_include.h
@@ -1,3 +1,4 @@
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -17,11 +18,11 @@
 void	print_bits(int octal);
 void	print_64bits(uint64_t octal);
 void	print_64bits_recursive(uint64_t octal);
-int     ft_astrcmp(char **s1, char **s2);
-void    ft_del(void *ap, size_t len);
+int		ft_astrcmp(char **s1, char **s2);
+void	ft_del(void *ap, size_t len);
 size_t	ft_lstlen(t_list *lst);
-void    ft_lstpush(t_list **list, t_list *new);
-void    ft_lstpop(t_list **list);
+void	ft_lstpush(t_list **list, t_list *new);
+void	ft_lstpop(t_list **list);
 void	**ft_lst2arr(t_list *lst);
 void	ft_error(const char *message);
 size_t	ft_arrlen(char **ar);
