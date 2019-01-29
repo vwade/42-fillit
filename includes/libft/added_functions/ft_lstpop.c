6ddddf44b68236e1a66e0f2c9c30b4fcb5662dcd
brayanmcz <brayan.mcz@gmail.com>
2019-01-28 19:46:42 -0800

    test

--- includes/libft/added_functions/ft_lstpop.c
+++ includes/libft/added_functions/ft_lstpop.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstpop.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
+/*   By: bcastro <bcastro@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 06:24:16 by viwade            #+#    #+#             */
-/*   Updated: 2019/01/15 06:31:12 by viwade           ###   ########.fr       */
+/*   Updated: 2019/01/28 19:44:48 by bcastro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@ static void
 	ft_memdel(&ap);
 }
 
-void    ft_lstpop(t_list **list)
+void
+	ft_lstpop(t_list **list)
 {
-    while (list[0]->next)
-        list[0] = list[0]->next;
-    ft_lstdel(&list[0], del);
+	while (list[0]->next)
+		list[0] = list[0]->next;
+	ft_lstdel(&list[0], del);
 }
